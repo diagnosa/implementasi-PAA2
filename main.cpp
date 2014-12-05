649a24c71e88707c0dbf4ffc2b5f0ec16b14772f
natsumiaya <natsumiaya@live.com>
2014-12-05 20:20:04 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int input[201];
-
-void randominput(int n)
+void randominput(int input[], int n)
 {
 	for (int i=0; i<n; i++)
 	{
@@ -11,9 +9,28 @@ void randominput(int n)
 	}
 }
 
+/*int PRTCTlj (int j, int v1, int v2, int p1j, int p2j) 
+{
+	int C1j = v1 + p1j + lj;
+	return (2 * (max(v2,C1j)) + p2j);
+}*/
+
+int ILS (int n)
+{
+	int maxiter = 120, pert = 7;
+	int iter = 1, cnt = 0, i = 1;
+	for (iter = 1; iter <= maxiter; iter++)
+	{
+		for (i = 1; i<=n; i++)
+		{
+			
+		}
+	}
+}
+
+
 int main() {
 	int n;
 	scanf("%d", &n);
-	randominput(n);
 	return 0;
 }
