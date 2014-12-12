239007172173b21f7c186322f5b9511b42d2f529
natsumiaya <natsumiaya@live.com>
2014-12-12 08:31:05 +0700

    Update main2.cpp
    input + initializing localsearch

--- main2.cpp
+++ main2.cpp
@@ -1,9 +1,46 @@
 #include <cstdio>
+#include <utility>
 #include <cstdlib>
-usig namespace std;
+using namespace std;
+
+//array start from 1 to n
+
+
+void generateinput(int n, int arr[])
+{
+  for (int i=1; i<=n; i++)
+  {
+    arr[i] = i;
+  }
+  for (int i=1;i<=n;i++)
+  {
+  	int j=rand()%n+1;
+  	swap (arr[i], arr[j]);	
+  }
+}
+
+void localsearchinsertion (int arr[])
+{
+  bool improve = 1;
+  while (improve)
+  {
+    improve = 0;
+    for (int i=0; i<=n; i++)
+    {
+      
+    }
+  }
+}
 
 int main ()
 {
   int n;
   scanf ("%d", &n);
+  int setofjob[n+1];
+  generateinput(n, setofjob);
+  for (int i=1; i<=n;i++)
+  {
+    printf ("%d ", setofjob[i]);
+  }
+  printf("\n");
 }
