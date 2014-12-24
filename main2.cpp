097eb8e0d01c2d36e45f96199d963e2fc22229ed
natsumiaya <natsumiaya@live.com>
2014-12-25 01:09:02 +0700

    mencari tau bagaimana membuat pi

--- main2.cpp
+++ main2.cpp
@@ -1,68 +1,58 @@
 #include <cstdio>
 #include <utility>
 #include <cstdlib>
-#include <vector>
 #include <ctime>
+#include <algorithm>
 using namespace std;
 
 //array start from 1 to n
+//test-case : mistic.heig-vd.ch/taillard/problemes.dir/ordonnancement.dir/flowshop.dir/tai50_20.txt
 
-struct setofjob
+//rule to sort descending
+bool desc (int a, int b)
 {
-	vector<int>start;
-	vector<int>finish;	
-};
-
-
-void generateinput(int n, setofjob *a)
-{
-	srand(time(NULL));
-	for (int i=1; i<=n; i++)
-	{
-		a->start[i] = rand()%1000;
-		a->finish[i] = a->start[i] + rand()%100;
-		if (a->start[i]==a->finish[i]) a->finish[i]+= a->start[i]+rand();
-	}
-  /*for (int i=1; i<=n; i++)
-  {
-    arr[i] = i;
-  }
-  for (int i=1;i<=n;i++)
-  {
-  	int j=rand()%n+1;
-  	swap (arr[i], arr[j]);	
-  }*/
+	return (a > b);
 }
 
-void localsearchinsertion (vector<int> &arr)
+//The FGH heuristic
+//possible pair processing-time > machine
+void createpi(int job[], int n)
+{
+	//sort descending
+	sort (job, job+n, desc);
+	
+	//Select the next job and insert it in all possible positions in the partial sequence 
+	//and keep the best one (i.e. minimum makespan) as the current partial sequence.
+	
+	
+}
+
+void localsearchinsertion (int pi[], int n)
 {
   bool improve = 1;
   while (improve)
   {
     improve = 0;
-   // for (int i=0; i<=n; i++)
-   // {
+    //remove a job k at random from pi (without repetition)
+    for (int i=0; i<=n; i++)
+   {
       
-   // }
+   }
   }
 }
 
 int main ()
 {
-  setofjob set;
   int n, m;
   printf ("Please input the number of job(s) : ");
   scanf ("%d", &n);
-  for(int i=0; i<=n; i++){
-  	set.start.push_back(0);
-  	set.finish.push_back(0);
+  int ptime[n+1];
+  for (int i=1; i<=n;i++)
+  {
+  	scanf("%d", ptime[i]);
   }
   printf ("Please input the number of machine(s) : ");
   scanf ("%d", &m);
-  generateinput(n, &set);
-  for (int i=1; i<=n;i++)
-  {
-    printf ("%d %d \n", set.start[i], set.finish[i]);
-  }
+  
   printf("\n");
 }
